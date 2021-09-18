441ade0d89de879708bbcb5c119b8301b428ec4c
Crowdbotics <team@crowdbotics.com>
2021-09-18 12:58:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "shiny_mud_30658Identifier",
+  url: "https://shiny_mud_30658Identifier.botics.co",
+  api: "https://shiny_mud_30658Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
